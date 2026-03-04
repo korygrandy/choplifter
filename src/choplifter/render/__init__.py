4865a1e1e7f188a7153d90f612a0cd73dab1504b
korygrandy <korygrandy@hotmail.com>
2026-03-04 09:05:56 -0600

    Refactor: split rendering into render package

---
+++ src/choplifter/render/__init__.py
@@ -0,0 +1,6 @@
+"""Internal rendering subpackage.
+
+This package exists to keep the public import surface stable via
+`choplifter.rendering` while allowing the implementation to be split into
+smaller modules.
+"""
